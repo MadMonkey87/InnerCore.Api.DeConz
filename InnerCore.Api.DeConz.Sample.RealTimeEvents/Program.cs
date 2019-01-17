390c0ae6ae78e7cafdf4494ec86fb59a261f18a1
Philippe Wechsler <myself@philippewechsler.ch>
2019-01-17 18:17:21 +0100

    sample application demonstrating listening to realtime events

---
+++ InnerCore.Api.DeConz.Sample.RealTimeEvents/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace InnerCore.Api.DeConz.Sample.RealTimeEvents
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            // Initialize the client
+
+            Console.WriteLine("ip (without port):");
+            var ip = Console.ReadLine();
+
+            Console.WriteLine("appkey:");
+            var appKey = Console.ReadLine();
+
+            var client = new DeConzClient(ip, appKey);
+
+            // test the connection
+
+            Console.WriteLine("connecting...");
+            var online = await client.CheckConnection();
+
+            if (!online)
+            {
+                Console.WriteLine("...unable to connect!");
+                Console.WriteLine("Press enter to quit");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("...connected successfully!");
+
+            // setup the events
+
+            client.SensorChanged += Client_SensorChanged;
+            client.ErrorEvent += Client_ErrorEvent;
+
+            // start listening to events (infinite as long as the server does not close the connection)
+
+            Console.WriteLine("Listening to events...");
+            await client.ListenToEvents();
+        }
+
+        private static void Client_SensorChanged(object sender, Models.WebSocket.SensorChangedEvent e)
+        {
+            if (e.Config != null)
+            {
+                Console.WriteLine($"Sensor {e.Id} has changed it's config");
+            }
+
+            if (e.State != null)
+            {
+                Console.WriteLine($"Sensor {e.Id} has changed it's state");
+            }
+        }
+
+        private static void Client_ErrorEvent(object sender, Models.WebSocket.ErrorEvent e)
+        {
+            var originalColor = Console.BackgroundColor;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine($"error while handling a message: {e.Ex.Message}");
+            Console.BackgroundColor = originalColor;
+        }
+    }
+}
