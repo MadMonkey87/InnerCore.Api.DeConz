e62b4d3d901091dc92ec9148be48b05de176eb90
Philippe Wechsler <myself@philippewechsler.ch>
2019-01-18 18:37:10 +0100

    identity device

--- InnerCore.Api.DeConz/DeConzClient-Touchlink.cs
+++ InnerCore.Api.DeConz/DeConzClient-Touchlink.cs
@@ -28,6 +28,30 @@ namespace InnerCore.Api.DeConz
         }
 
         /// <summary>
+        /// Puts a device into identify mode for example a light will blink a few times.
+        /// </summary>
+        /// <param name="id">id must be one of the indentifiers which are returned in the scan result</param>
+        /// <returns></returns>
+        public async Task<DeConzResults> Identity(string id)
+        {
+            if (id == null)
+                throw new ArgumentNullException(nameof(id));
+            if (id.Trim() == String.Empty)
+                throw new ArgumentException("id can not be empty or a blank string", nameof(id));
+
+            CheckInitialized();
+
+            string command = JsonConvert.SerializeObject(new { }, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore });
+
+            HttpClient client = await GetHttpClient().ConfigureAwait(false);
+            var result = await client.PostAsync(new Uri(string.Format("{0}touchlink/{1}/identify", ApiBase, id)), new JsonContent(command)).ConfigureAwait(false);
+
+            string jsonResult = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return DeserializeDefaultDeConzResult(jsonResult);
+        }
+
+        /// <summary>
         /// Send a reset to factory new request to a device.
         /// </summary>
         /// <param name="id">id must be one of the indentifiers which are returned in the scan result</param>
