977ed4d944febffd1b2f67feb360eeea7934cf9d
Philippe Wechsler <myself@philippewechsler.ch>
2019-01-22 23:46:45 +0100

    provide schedule status as enum

---
+++ InnerCore.Api.DeConz/Models/Schedule/ScheduleStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace InnerCore.Api.DeConz.Models.Schedule
+{
+    public enum ScheduleStatus
+    {
+        [EnumMember(Value = "enabled")]
+        Enabled,
+        [EnumMember(Value = "disabled")]
+        Disabled,
+    }
+}
