b0cf14373d4376c5195b494c9c405e97915200b2
Philippe Wechsler <myself@philippewechsler.ch>
2019-01-24 19:06:40 +0100

    provide all details the bridge full state offers (scenes, sensors, rules & schedules)

--- InnerCore.Api.DeConz/Models/Bridge/Bridge.cs
+++ InnerCore.Api.DeConz/Models/Bridge/Bridge.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using InnerCore.Api.DeConz.Models.Lights;
+using InnerCore.Api.DeConz.Models.Sensors;
+using InnerCore.Api.DeConz.Models.Rules;
 
 namespace InnerCore.Api.DeConz.Models.Bridge
 {
@@ -19,16 +21,57 @@ namespace InnerCore.Api.DeConz.Models.Bridge
             Config = bridge.Config;
 
             foreach (var light in bridge.Lights)
+            {
                 light.Value.Id = light.Key;
+            }
             Lights = bridge.Lights.Select(l => l.Value).ToList();
 
+            foreach (var sensor in bridge.Sensors)
+            {
+                sensor.Value.Id = sensor.Key;
+            }
+            Sensors = bridge.Sensors.Select(s => s.Value).ToList();
+
             foreach (var group in bridge.Groups)
+            {
                 group.Value.Id = group.Key;
+            }
             Groups = bridge.Groups.Select(l => l.Value).ToList();
 
-            //Fix whitelist IDs
+
+
+
+
+
+            foreach (var scene in bridge.Scenes)
+            {
+                scene.Value.Id = scene.Key;
+            }
+            Scenes = bridge.Scenes.Select(s => s.Value).ToList();
+
+            foreach (var rule in bridge.Rules)
+            {
+                rule.Value.Id = rule.Key;
+            }
+            Rules = bridge.Rules.Select(r => r.Value).ToList();
+
+            foreach (var schedule in bridge.Schedules)
+            {
+                schedule.Value.Id = schedule.Key;
+            }
+            Schedules = bridge.Schedules.Select(s => s.Value).ToList();
+
+
+
+
+
+
+
+
             foreach (var whitelist in bridge.Config.WhiteList)
+            {
                 whitelist.Value.Id = whitelist.Key;
+            }
             WhiteList = bridge.Config.WhiteList.Select(l => l.Value).ToList();
         }
 
@@ -36,6 +79,11 @@ namespace InnerCore.Api.DeConz.Models.Bridge
         /// Light info from the bridge
         /// </summary>
         public IEnumerable<Light> Lights { get; private set; }
+
+        /// <summary>
+        /// Sensor info from the bridge
+        /// </summary>
+        public IEnumerable<Sensor> Sensors { get; private set; }
 
         /// <summary>
         /// Group info from the bridge
@@ -46,6 +94,21 @@ namespace InnerCore.Api.DeConz.Models.Bridge
         /// Bridge config info
         /// </summary>
         public BridgeConfig Config { get; private set; }
+
+        /// <summary>
+        /// Schedule info from the bridge
+        /// </summary>
+        public IEnumerable<Schedule.Schedule> Schedules { get; private set; }
+
+        /// <summary>
+        /// Rule info from the bridge
+        /// </summary>
+        public IEnumerable<Rule> Rules { get; private set; }
+
+        /// <summary>
+        /// Scene info from the bridge
+        /// </summary>
+        public IEnumerable<Scenes.Scene> Scenes { get; private set; }
 
         /// <summary>
         /// Light info from the bridge
--- InnerCore.Api.DeConz/Models/Bridge/BridgeState.cs
+++ InnerCore.Api.DeConz/Models/Bridge/BridgeState.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using InnerCore.Api.DeConz.Models.Groups;
 using InnerCore.Api.DeConz.Models.Lights;
+using InnerCore.Api.DeConz.Models.Rules;
+using InnerCore.Api.DeConz.Models.Sensors;
 
 namespace InnerCore.Api.DeConz.Models.Bridge
 {
@@ -11,9 +13,17 @@ namespace InnerCore.Api.DeConz.Models.Bridge
     {
         public Dictionary<string, Light> Lights { get; set; }
 
+        public Dictionary<string, Sensor> Sensors { get; set; }
+
         public Dictionary<string, Group> Groups { get; set; }
 
         public BridgeConfig Config { get; set; }
+
+        public Dictionary<string, Schedule.Schedule> Schedules { get; set; }
+
+        public Dictionary<string, Scenes.Scene> Scenes { get; set; }
+
+        public Dictionary<string, Rule> Rules { get; set; }
 
         public Dictionary<string, WhiteList> Whitelist { get; set; }
     }
